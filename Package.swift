be8ed450d29d29d7b47e7f768658fde7d35be6a3
Fabian Boemer <fabian_boemer@apple.com>
2024-07-16 13:46:23 -0700

    Move swift-docc-plugin back to swiftlang (#17)

--- Package.swift
+++ Package.swift
@@ -50,11 +50,10 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.4.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
-        // Keep version in sync with README
-        .package(url: "https://github.com/apple/swift-protobuf", from: "1.27.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-numerics", from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-protobuf", from: "1.27.0"), // Keep version in sync with README
+        .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.

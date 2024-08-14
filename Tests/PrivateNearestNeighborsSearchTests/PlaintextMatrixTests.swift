0fd2cf3f5a82b93be44b07f42c3b158a529b3c1f
Fabian Boemer <fabian_boemer@apple.com>
2024-08-14 15:30:58 -0700

    Implement PlaintextMatrix.denseColumn encoding (#59)

--- Tests/PrivateNearestNeighborsSearchTests/PlaintextMatrixTests.swift
+++ Tests/PrivateNearestNeighborsSearchTests/PlaintextMatrixTests.swift
@@ -114,45 +114,150 @@ final class PlaintextMatrixTests: XCTestCase {
         try runTest(Bfv<UInt32>.self)
     }
 
-    func testPlaintextMatrixDenseRow() throws {
-        func runTest<Scheme: HeScheme>(
-            context: Context<Scheme>,
-            dimensions: MatrixDimensions,
-            expected: [[Int]]) throws
-        {
-            guard context.supportsSimdEncoding else {
-                return
-            }
-            let t = context.plaintextModulus
-            let expected: [[Scheme.Scalar]] = expected.map { row in row.map { Scheme.Scalar($0) % t } }
-            let encodeValues: [[Scheme.Scalar]] = (0..<dimensions.rowCount).map { rowIndex in
-                (0..<dimensions.columnCount).map { columnIndex in
-                    let value = 1 + Scheme.Scalar(rowIndex * dimensions.columnCount + columnIndex)
-                    return value % t
-                }
-            }
-            let packing = PlaintextMatrixPacking.denseRow
-
-            let plaintextMatrix = try PlaintextMatrix<Scheme, Coeff>(
-                context: context,
-                dimensions: dimensions,
-                packing: packing,
-                values: encodeValues.flatMap { $0 })
-            XCTAssertEqual(plaintextMatrix.rowCount, dimensions.rowCount)
-            XCTAssertEqual(plaintextMatrix.columnCount, dimensions.columnCount)
-            XCTAssertEqual(plaintextMatrix.packing, packing)
-            XCTAssertEqual(plaintextMatrix.context, context)
-            // Test round-trip
-            XCTAssertEqual(try plaintextMatrix.unpack(), encodeValues.flatMap { $0 })
-
-            // Test representation
-            XCTAssertEqual(plaintextMatrix.plaintexts.count, expected.count)
-            for (plaintext, expected) in zip(plaintextMatrix.plaintexts, expected) {
-                let decoded: [Scheme.Scalar] = try plaintext.decode(format: .simd)
-                XCTAssertEqual(decoded, expected)
+    private func runPlaintextMatrixInitTest<Scheme: HeScheme>(
+        context: Context<Scheme>,
+        dimensions: MatrixDimensions,
+        packing: PlaintextMatrixPacking,
+        expected: [[Int]]) throws
+    {
+        guard context.supportsSimdEncoding else {
+            return
+        }
+        let t = context.plaintextModulus
+        let expected: [[Scheme.Scalar]] = expected.map { row in row.map { Scheme.Scalar($0) % t } }
+        let encodeValues: [[Scheme.Scalar]] = (0..<dimensions.rowCount).map { rowIndex in
+            (0..<dimensions.columnCount).map { columnIndex in
+                let value = 1 + Scheme.Scalar(rowIndex * dimensions.columnCount + columnIndex)
+                return value % t
             }
         }
+        let plaintextMatrix = try PlaintextMatrix<Scheme, Coeff>(
+            context: context,
+            dimensions: dimensions,
+            packing: packing,
+            values: encodeValues.flatMap { $0 })
+        XCTAssertEqual(plaintextMatrix.rowCount, dimensions.rowCount)
+        XCTAssertEqual(plaintextMatrix.columnCount, dimensions.columnCount)
+        XCTAssertEqual(plaintextMatrix.packing, packing)
+        XCTAssertEqual(plaintextMatrix.context, context)
+        // Test round-trip
+        XCTAssertEqual(try plaintextMatrix.unpack(), encodeValues.flatMap { $0 })
 
+        // Test representation
+        XCTAssertEqual(plaintextMatrix.plaintexts.count, expected.count)
+        for (plaintext, expected) in zip(plaintextMatrix.plaintexts, expected) {
+            let decoded: [Scheme.Scalar] = try plaintext.decode(format: .simd)
+            XCTAssertEqual(decoded, expected)
+        }
+    }
+
+    func testPlaintextMatrixDenseColumn() throws {
+        let kats: [((rowCount: Int, columnCount: Int), expected: [[Int]])] = [
+            ((1, 1), [[1, 0, 0, 0, 0, 0, 0, 0]]),
+            ((1, 2), [[1, 2, 0, 0, 0, 0, 0, 0]]),
+            ((1, 3), [[1, 2, 3, 0, 0, 0, 0, 0]]),
+            ((1, 4), [[1, 2, 3, 4, 0, 0, 0, 0]]),
+            ((1, 5), [[1, 2, 3, 4, 5, 0, 0, 0]]),
+            ((1, 6), [[1, 2, 3, 4, 5, 6, 0, 0]]),
+            ((1, 7), [[1, 2, 3, 4, 5, 6, 7, 0]]),
+            ((1, 8), [[1, 2, 3, 4, 5, 6, 7, 8]]),
+            ((1, 9), [[1, 2, 3, 4, 5, 6, 7, 8],
+                      [9, 0, 0, 0, 0, 0, 0, 0]]),
+            ((2, 1), [[1, 2, 0, 0, 0, 0, 0, 0]]),
+            ((2, 2), [[1, 3, 2, 4, 0, 0, 0, 0]]),
+            // extra 0 prevents column from spanning multiple SIMD rows.
+            ((2, 3), [[1, 4, 2, 5, 3, 6, 0, 0]]),
+            ((2, 4), [[1, 5, 2, 6, 3, 7, 4, 8]]),
+            ((2, 5), [[1, 6, 2, 7, 3, 8, 4, 9],
+                      [5, 10, 0, 0, 0, 0, 0, 0]]),
+            ((2, 6), [[1, 7, 2, 8, 3, 9, 4, 10],
+                      [5, 11, 6, 12, 0, 0, 0, 0]]),
+            ((2, 7), [[1, 8, 2, 9, 3, 10, 4, 11],
+                      [5, 12, 6, 13, 7, 14, 0, 0]]),
+            ((2, 8), [[1, 9, 2, 10, 3, 11, 4, 12],
+                      [5, 13, 6, 14, 7, 15, 8, 16]]),
+            ((2, 9), [[1, 10, 2, 11, 3, 12, 4, 13],
+                      [5, 14, 6, 15, 7, 16, 8, 17],
+                      [9, 18, 0, 0, 0, 0, 0, 0]]),
+            ((3, 1), [[1, 2, 3, 0, 0, 0, 0, 0]]),
+            // extra 0 prevents column from spanning multiple SIMD rows.
+            ((3, 2), [[1, 3, 5, 0, 2, 4, 6, 0]]),
+            // extra 0 prevents column from spanning multiple SIMD rows.
+            ((3, 3), [[1, 4, 7, 0, 2, 5, 8, 0],
+                      [3, 6, 9, 0, 0, 0, 0, 0]]),
+            // extra 0 prevents column from spanning multiple SIMD rows.
+            ((3, 4), [[1, 5, 9, 0, 2, 6, 10, 0],
+                      [3, 7, 11, 0, 4, 8, 12, 0]]),
+            ((4, 1), [[1, 2, 3, 4, 0, 0, 0, 0]]),
+            ((4, 2), [[1, 3, 5, 7, 2, 4, 6, 8]]),
+            ((4, 3), [[1, 4, 7, 10, 2, 5, 8, 11],
+                      [3, 6, 9, 12, 0, 0, 0, 0]]),
+            ((4, 4), [[1, 5, 9, 13, 2, 6, 10, 14],
+                      [3, 7, 11, 15, 4, 8, 12, 16]]),
+            ((4, 5), [[1, 6, 11, 16, 2, 7, 12, 17],
+                      [3, 8, 13, 18, 4, 9, 14, 19],
+                      [5, 10, 15, 20, 0, 0, 0, 0]]),
+            ((4, 6), [[1, 7, 13, 19, 2, 8, 14, 20],
+                      [3, 9, 15, 21, 4, 10, 16, 22],
+                      [5, 11, 17, 23, 6, 12, 18, 24]]),
+            ((5, 1), [[1, 2, 3, 4, 5, 0, 0, 0]]),
+            ((5, 2), [[1, 3, 5, 7, 9, 0, 0, 0],
+                      [2, 4, 6, 8, 10, 0, 0, 0]]),
+            ((5, 3), [[1, 4, 7, 10, 13, 0, 0, 0],
+                      [2, 5, 8, 11, 14, 0, 0, 0],
+                      [3, 6, 9, 12, 15, 0, 0, 0]]),
+            ((5, 4), [[1, 5, 9, 13, 17, 0, 0, 0],
+                      [2, 6, 10, 14, 18, 0, 0, 0],
+                      [3, 7, 11, 15, 19, 0, 0, 0],
+                      [4, 8, 12, 16, 20, 0, 0, 0]]),
+            ((5, 5), [[1, 6, 11, 16, 21, 0, 0, 0],
+                      [2, 7, 12, 17, 22, 0, 0, 0],
+                      [3, 8, 13, 18, 23, 0, 0, 0],
+                      [4, 9, 14, 19, 24, 0, 0, 0],
+                      [5, 10, 15, 20, 25, 0, 0, 0]]),
+            ((10, 5), [[1, 6, 11, 16, 21, 26, 31, 36], [41, 46, 0, 0, 0, 0, 0, 0],
+                       [2, 7, 12, 17, 22, 27, 32, 37], [42, 47, 0, 0, 0, 0, 0, 0],
+                       [3, 8, 13, 18, 23, 28, 33, 38], [43, 48, 0, 0, 0, 0, 0, 0],
+                       [4, 9, 14, 19, 24, 29, 34, 39], [44, 49, 0, 0, 0, 0, 0, 0],
+                       [5, 10, 15, 20, 25, 30, 35, 40], [45, 50, 0, 0, 0, 0, 0, 0]]),
+            ((10, 10), [[1, 11, 21, 31, 41, 51, 61, 71], [81, 91, 0, 0, 0, 0, 0, 0],
+                        [2, 12, 22, 32, 42, 52, 62, 72], [82, 92, 0, 0, 0, 0, 0, 0],
+                        [3, 13, 23, 33, 43, 53, 63, 73], [83, 93, 0, 0, 0, 0, 0, 0],
+                        [4, 14, 24, 34, 44, 54, 64, 74], [84, 94, 0, 0, 0, 0, 0, 0],
+                        [5, 15, 25, 35, 45, 55, 65, 75], [85, 95, 0, 0, 0, 0, 0, 0],
+                        [6, 16, 26, 36, 46, 56, 66, 76], [86, 96, 0, 0, 0, 0, 0, 0],
+                        [7, 17, 27, 37, 47, 57, 67, 77], [87, 97, 0, 0, 0, 0, 0, 0],
+                        [8, 18, 28, 38, 48, 58, 68, 78], [88, 98, 0, 0, 0, 0, 0, 0],
+                        [9, 19, 29, 39, 49, 59, 69, 79], [89, 99, 0, 0, 0, 0, 0, 0],
+                        [10, 20, 30, 40, 50, 60, 70, 80], [90, 100, 0, 0, 0, 0, 0, 0]]),
+        ]
+
+        func runTest<Scheme: HeScheme>(for _: Scheme.Type) throws {
+            let encryptionParams = try EncryptionParameters<Scheme>(
+                polyDegree: 8,
+                plaintextModulus: 1153,
+                coefficientModuli: Scheme.Scalar
+                    .generatePrimes(
+                        significantBitCounts: [25, 25],
+                        preferringSmall: false,
+                        nttDegree: 8),
+                errorStdDev: .stdDev32,
+                securityLevel: .unchecked)
+            let context = try Context(encryptionParameters: encryptionParams)
+            for ((rowCount, columnCount), expected) in kats {
+                let dimensions = try MatrixDimensions(rowCount: rowCount, columnCount: columnCount)
+                try runPlaintextMatrixInitTest(
+                    context: context,
+                    dimensions: dimensions,
+                    packing: .denseColumn, expected: expected)
+            }
+        }
+        try runTest(for: NoOpScheme.self)
+        try runTest(for: Bfv<UInt32>.self)
+        try runTest(for: Bfv<UInt64>.self)
+    }
+
+    func testPlaintextMatrixDenseRow() throws {
         let kats: [((rowCount: Int, columnCount: Int), expected: [[Int]])] = [
             ((1, 1), [[1, 1, 1, 1, 1, 1, 1, 1]]),
             ((1, 2), [[1, 2, 1, 2, 1, 2, 1, 2]]),
@@ -223,7 +328,11 @@ final class PlaintextMatrixTests: XCTestCase {
             let context = try Context(encryptionParameters: encryptionParams)
             for ((rowCount, columnCount), expected) in kats {
                 let dimensions = try MatrixDimensions(rowCount: rowCount, columnCount: columnCount)
-                try runTest(context: context, dimensions: dimensions, expected: expected)
+                try runPlaintextMatrixInitTest(
+                    context: context,
+                    dimensions: dimensions,
+                    packing: .denseRow,
+                    expected: expected)
             }
         }
         try runTest(for: NoOpScheme.self)

a2d371e4a456e1aff67981d42a61beccd1309daa
Fabian Boemer <fabian_boemer@apple.com>
2024-08-19 13:22:23 -0700

    Implement CiphertextMatrix/extractDenseRow

--- Sources/PrivateNearestNeighborsSearch/CiphertextMatrix.swift
+++ Sources/PrivateNearestNeighborsSearch/CiphertextMatrix.swift
@@ -16,17 +16,14 @@ import HomomorphicEncryption
 
 /// Stores a matrix of scalars as ciphertexts.
 struct CiphertextMatrix<Scheme: HeScheme, Format: PolyFormat>: Equatable, Sendable {
-    typealias Packing = PlaintextMatrixPacking
-    typealias Dimensions = MatrixDimensions
-
     /// Dimensions of the scalars.
-    @usableFromInline let dimensions: Dimensions
+    @usableFromInline let dimensions: MatrixDimensions
 
     /// Dimensions of the scalar matrix in a SIMD-encoded plaintext.
     @usableFromInline let simdDimensions: SimdEncodingDimensions
 
     /// Plaintext packing with which the data is stored.
-    @usableFromInline let packing: Packing
+    @usableFromInline let packing: MatrixPacking
 
     /// Encrypted data.
     @usableFromInline let ciphertexts: [Ciphertext<Scheme, Format>]
@@ -59,7 +56,7 @@ struct CiphertextMatrix<Scheme: HeScheme, Format: PolyFormat>: Equatable, Sendab
     ///   - ciphertexts: ciphertexts encrypting the data; must not be empty.
     /// - Throws: Error upon failure to initialize the ciphertext matrix.
     @inlinable
-    init(dimensions: Dimensions, packing: Packing, ciphertexts: [Ciphertext<Scheme, Format>]) throws {
+    init(dimensions: MatrixDimensions, packing: MatrixPacking, ciphertexts: [Ciphertext<Scheme, Format>]) throws {
         guard let context = ciphertexts.first?.context else {
             throw PnnsError.emptyCiphertextArray
         }
@@ -126,5 +123,157 @@ extension CiphertextMatrix {
             dimensions: dimensions,
             packing: packing,
             ciphertexts: coeffCiphertexts)
+    }
+}
+
+extension CiphertextMatrix {
+    /// Computes the evaluation key configuration for calling `extractDenseRow`.
+    /// - Parameters:
+    ///   - encryptionParams: Encryption parameters; must support `.simd` encoding.
+    ///   - dimensions: Dimensions of the matrix to call `extractDenseRow` on.
+    /// - Returns: The evaluation key configuration.
+    /// - Throws: Error upon failure to generate the evaluation key configuration.
+    @inlinable
+    static func extractDenseRowConfig(for encryptionParams: EncryptionParameters<Scheme>,
+                                      dimensions: MatrixDimensions) throws -> EvaluationKeyConfiguration
+    {
+        if dimensions.rowCount == 1 {
+            // extractDenseRow is a No-op, so no evaluation key required
+            return EvaluationKeyConfiguration()
+        }
+        guard let simdDimensions = encryptionParams.simdDimensions else {
+            throw PnnsError.simdEncodingNotSupported(for: encryptionParams)
+        }
+        let degree = encryptionParams.polyDegree
+        var galoisElements = [GaloisElement.swappingRows(degree: degree)]
+        let columnCountPowerOfTwo = dimensions.columnCount.nextPowerOfTwo
+        if columnCountPowerOfTwo != simdDimensions.columnCount {
+            try galoisElements.append(GaloisElement.rotatingColumns(by: columnCountPowerOfTwo, degree: degree))
+        }
+        return EvaluationKeyConfiguration(galoisElements: galoisElements)
+    }
+
+    /// Extracts a ciphertext matrix with a single row and `.denseRow` packing.
+    /// - Parameters:
+    ///   - rowIndex: Row index to extract
+    ///   - evaluationKey: Evaluation key; must have `CiphertextMatrix/extractDenseRow` configuration
+    /// - Returns: A ciphertext matrix in `.denseRow` format with 1 row
+    /// - Throws: Error upon failure to extract the row.
+    @inlinable
+    func extractDenseRow(rowIndex: Int, evaluationKey: EvaluationKey<Scheme>) throws -> Self
+        where Format == Scheme.CanonicalCiphertextFormat
+    {
+        precondition((0..<dimensions.rowCount).contains(rowIndex))
+        guard packing == .denseRow else {
+            throw PnnsError.wrongMatrixPacking(got: packing, expected: .denseRow)
+        }
+        precondition(simdDimensions.rowCount == 2, "SIMD row count must be 2")
+
+        let columnCountPowerOfTwo = dimensions.columnCount.nextPowerOfTwo
+        let degree = context.degree.nextPowerOfTwo
+        let rowsPerSimdRow = simdDimensions.columnCount / columnCount
+        let rowsPerCiphertext = rowsPerSimdRow * simdDimensions.rowCount
+        let ciphertextIndex = rowIndex / rowsPerCiphertext
+        if rowCount == 1 {
+            return self
+        }
+
+        // Suppose, e.g., N=16, columnCount = 2, and the ciphertext data encrypts 2 rows: [1, 2] and [3, 4].
+        // These rows are packed in the ciphertext SIMD simd rows as
+        // [[1, 2, 3, 4, 1, 2, 3, 4],
+        //  [1, 2, 3, 4, 1, 2, 3, 4]].
+        // Suppose ciphertextRowIndex == 1, i.e., we want to return an encryption of
+        // [[3, 4, 3, 4, 3, 4, 3, 4], [3, 4, 3, 4, 3, 4, 3, 4]]
+
+        //  Returns the SIMD slot indices for the `rowIndex`'th row of the ciphertext matrix.
+        func simdSlotIndices(rowIndex: Int) -> Range<Int> {
+            precondition((0..<dimensions.rowCount).contains(rowIndex))
+            let ciphertextRowIndex = rowIndex % rowsPerCiphertext
+            let batchStart = ciphertextRowIndex * columnCountPowerOfTwo
+            var batchIndices = (batchStart..<batchStart + columnCountPowerOfTwo)
+            // Ensure no repeated values span multiple SIMD rows
+            let overflowsSimdRow = batchIndices.contains(simdDimensions.columnCount)
+            if overflowsSimdRow {
+                batchIndices = (simdDimensions.columnCount..<simdDimensions.columnCount + columnCountPowerOfTwo)
+            } else if batchIndices.upperBound > simdDimensions.columnCount {
+                let padding = simdColumnCount % columnCountPowerOfTwo
+                batchIndices = (batchIndices.startIndex + padding..<batchIndices.endIndex + padding)
+            }
+            // The last ciphertext pads until the end of the ciphertext.
+            if ciphertextIndex == ciphertexts.indices.last {
+                let upperBound = batchIndices.endIndex.nextMultiple(of: simdDimensions.columnCount, variableTime: true)
+                batchIndices = batchIndices.startIndex..<upperBound
+            }
+            return batchIndices
+        }
+        let batchIndices = simdSlotIndices(rowIndex: rowIndex)
+
+        // The number of rows in covered by `batchIndices`
+        let rowCountInBatch = {
+            var lastRowIndexInBatch = rowIndex + 1
+            while lastRowIndexInBatch < dimensions.rowCount,
+                  simdSlotIndices(rowIndex: lastRowIndexInBatch).upperBound == batchIndices.upperBound
+            {
+                lastRowIndexInBatch += 1
+            }
+            var firstRowIndexInBatch = rowIndex > 0 ? rowIndex - 1 : 0
+            while firstRowIndexInBatch > 0,
+                  simdSlotIndices(rowIndex: firstRowIndexInBatch).upperBound == batchIndices.upperBound
+            {
+                firstRowIndexInBatch -= 1
+            }
+            return lastRowIndexInBatch - firstRowIndexInBatch
+        }()
+
+        // First, we mask out just the ciphertext data row vector e.g.,
+        // plaintextMask = [[0, 0, 1, 1, 0, 0, 1, 1],
+        //                  [0, 0, 0, 0, 0, 0, 0, 0]]
+        let (plaintextMask, copiesInMask) = try {
+            var repeatMask = Array(repeating: Scheme.Scalar(1), count: columnCountPowerOfTwo)
+            repeatMask += Array(repeating: 0, count: columnCountPowerOfTwo * (rowCountInBatch - 1))
+            // pad to next power of two
+            repeatMask += Array(repeating: 0, count: repeatMask.count.nextPowerOfTwo - repeatMask.count)
+
+            var mask = Array(repeating: Scheme.Scalar(0), count: batchIndices.lowerBound)
+            var repeatCountInMask = 0
+            while mask.count < batchIndices.upperBound {
+                mask += repeatMask
+                repeatCountInMask += 1
+            }
+            mask = Array(mask.prefix(degree))
+            let plaintext: Plaintext<Scheme, Eval> = try context.encode(values: mask, format: .simd)
+            return (plaintext, repeatCountInMask)
+        }()
+
+        var ciphertextEval = try ciphertexts[ciphertextIndex].convertToEvalFormat()
+        try ciphertextEval *= plaintextMask
+        var ciphertext = try ciphertextEval.convertToCanonicalFormat()
+        // e.g., `ciphertext` now encrypts
+        // [[0, 0, 3, 4, 0, 0, 3, 4],
+        //  [0, 0, 0, 0, 0, 0, 0, 0]]
+
+        // Replicate the values across one SIMD row by rotating and adding.
+        let rotateCount = simdColumnCount / (copiesInMask * columnCountPowerOfTwo) - 1
+        var ciphertextCopyRight = ciphertext
+        for _ in 0..<rotateCount {
+            try ciphertextCopyRight.rotateColumns(by: columnCountPowerOfTwo, using: evaluationKey)
+            try ciphertext += ciphertextCopyRight
+        }
+        // e.g., `ciphertext` now encrypts
+        // [[3, 4, 3, 4, 3, 4, 3, 4],
+        //  [0, 0, 0, 0, 0, 0, 0, 0]]
+
+        // Duplicate values to both SIMD rows
+        var ciphertextCopy = ciphertext
+        try ciphertextCopy.swapRows(using: evaluationKey)
+        try ciphertext += ciphertextCopy
+        // e.g., `ciphertext` now encrypts
+        // [[3, 4, 3, 4, 3, 4, 3, 4],
+        //  [3, 4, 3, 4, 3, 4, 3, 4]]
+
+        return try CiphertextMatrix(
+            dimensions: MatrixDimensions(rowCount: 1, columnCount: columnCount),
+            packing: packing,
+            ciphertexts: [ciphertext])
     }
 }
--- Tests/PrivateNearestNeighborsSearchTests/CiphertextMatrixTests.swift
+++ Tests/PrivateNearestNeighborsSearchTests/CiphertextMatrixTests.swift
@@ -17,6 +17,15 @@ import HomomorphicEncryption
 import TestUtilities
 import XCTest
 
+func increasingData<T: ScalarType>(dimensions: MatrixDimensions, modulus: T) -> [[T]] {
+    (0..<dimensions.rowCount).map { rowIndex in
+        (0..<dimensions.columnCount).map { columnIndex in
+            let value = 1 + T(rowIndex * dimensions.columnCount + columnIndex)
+            return value % modulus
+        }
+    }
+}
+
 final class CiphertextMatrixTests: XCTestCase {
     func testEncryptDecryptRoundTrip() throws {
         func runTest<Scheme: HeScheme>(for _: Scheme.Type) throws {
@@ -25,12 +34,9 @@ final class CiphertextMatrixTests: XCTestCase {
             XCTAssert(encryptionParams.supportsSimdEncoding)
             let context = try Context<Scheme>(encryptionParameters: encryptionParams)
             let dimensions = try MatrixDimensions(rowCount: 10, columnCount: 4)
-            let encodeValues: [[Scheme.Scalar]] = (0..<dimensions.rowCount).map { rowIndex in
-                (0..<dimensions.columnCount).map { columnIndex in
-                    let value = 1 + Scheme.Scalar(rowIndex * dimensions.columnCount + columnIndex)
-                    return value % context.plaintextModulus
-                }
-            }
+            let encodeValues: [[Scheme.Scalar]] = increasingData(
+                dimensions: dimensions,
+                modulus: context.plaintextModulus)
             let plaintextMatrix = try PlaintextMatrix<Scheme, Coeff>(
                 context: context,
                 dimensions: dimensions,
@@ -53,12 +59,9 @@ final class CiphertextMatrixTests: XCTestCase {
             XCTAssert(encryptionParams.supportsSimdEncoding)
             let context = try Context<Scheme>(encryptionParameters: encryptionParams)
             let dimensions = try MatrixDimensions(rowCount: 10, columnCount: 4)
-            let encodeValues: [[Scheme.Scalar]] = (0..<dimensions.rowCount).map { rowIndex in
-                (0..<dimensions.columnCount).map { columnIndex in
-                    let value = 1 + Scheme.Scalar(rowIndex * dimensions.columnCount + columnIndex)
-                    return value % context.plaintextModulus
-                }
-            }
+            let encodeValues: [[Scheme.Scalar]] = increasingData(
+                dimensions: dimensions,
+                modulus: context.plaintextModulus)
             let plaintextMatrix = try PlaintextMatrix<Scheme, Coeff>(
                 context: context,
                 dimensions: dimensions,
@@ -70,6 +73,82 @@ final class CiphertextMatrixTests: XCTestCase {
             let ciphertextMatrixRoundTrip = try ciphertextEvalMatrix.convertToCoeffFormat()
             let decoded = try ciphertextMatrixRoundTrip.decrypt(using: secretKey)
             XCTAssertEqual(plaintextMatrix, decoded)
+        }
+        try runTest(for: NoOpScheme.self)
+        try runTest(for: Bfv<UInt32>.self)
+        try runTest(for: Bfv<UInt64>.self)
+    }
+
+    func testExtractDenseRow() throws {
+        func runTest<Scheme: HeScheme>(for _: Scheme.Type) throws {
+            let degree = 16
+            let plaintextModulus = try Scheme.Scalar.generatePrimes(
+                significantBitCounts: [9],
+                preferringSmall: true,
+                nttDegree: degree)[0]
+            let coefficientModuli = try Scheme.Scalar.generatePrimes(
+                significantBitCounts: Array(
+                    repeating: Scheme.Scalar.bitWidth - 4,
+                    count: 2),
+                preferringSmall: false,
+                nttDegree: degree)
+            let encryptionParams = try EncryptionParameters<Scheme>(
+                polyDegree: degree,
+                plaintextModulus: plaintextModulus,
+                coefficientModuli: coefficientModuli,
+                errorStdDev: .stdDev32,
+                securityLevel: .unchecked)
+            XCTAssert(encryptionParams.supportsSimdEncoding)
+            let context = try Context<Scheme>(encryptionParameters: encryptionParams)
+
+            for rowCount in 1..<(2 * degree) {
+                for columnCount in 1..<degree / 2 {
+                    let dimensions = try MatrixDimensions(
+                        rowCount: rowCount,
+                        columnCount: columnCount)
+                    let encodeValues: [[Scheme.Scalar]] = increasingData(
+                        dimensions: dimensions,
+                        modulus: plaintextModulus)
+
+                    let plaintextMatrix = try PlaintextMatrix<Scheme, Coeff>(
+                        context: context,
+                        dimensions: dimensions,
+                        packing: .denseRow,
+                        values: encodeValues.flatMap { $0 })
+                    let secretKey = try context.generateSecretKey()
+                    let ciphertextMatrix: CiphertextMatrix = try plaintextMatrix.encrypt(using: secretKey)
+
+                    let evaluationKeyConfig = try CiphertextMatrix<Scheme, Coeff>.extractDenseRowConfig(
+                        for: encryptionParams,
+                        dimensions: dimensions)
+                    let evaluationKey = try context.generateEvaluationKey(
+                        configuration: evaluationKeyConfig,
+                        using: secretKey)
+
+                    for rowIndex in 0..<rowCount {
+                        let extractedRow = try ciphertextMatrix.extractDenseRow(
+                            rowIndex: rowIndex,
+                            evaluationKey: evaluationKey)
+
+                        let expectedDimensions = try MatrixDimensions(
+                            rowCount: 1,
+                            columnCount: columnCount)
+                        XCTAssertEqual(extractedRow.dimensions, expectedDimensions)
+
+                        // Check unpacking
+                        let decrypted = try extractedRow.decrypt(using: secretKey)
+                        let unpacked: [Scheme.Scalar] = try decrypted.unpack()
+                        XCTAssertEqual(unpacked, encodeValues[rowIndex])
+
+                        // Check encoded values
+                        var row = encodeValues[rowIndex]
+                        row += Array(repeating: 0, count: row.count.nextPowerOfTwo - row.count)
+                        let expectedRow = Array(repeating: row, count: degree / row.count).flatMap { $0 }
+                        let decoded: [Scheme.Scalar] = try decrypted.plaintexts[0].decode(format: .simd)
+                        XCTAssertEqual(decoded, expectedRow)
+                    }
+                }
+            }
         }
         try runTest(for: NoOpScheme.self)
         try runTest(for: Bfv<UInt32>.self)

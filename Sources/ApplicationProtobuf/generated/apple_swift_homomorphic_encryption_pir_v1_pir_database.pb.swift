1596df5e8a261cde8c36c724af7bd018eca55305
Fabian Boemer <fabian_boemer@apple.com>
2026-01-14 12:21:28 -0800

    Regnerate proto files

--- Sources/ApplicationProtobuf/generated/apple_swift_homomorphic_encryption_pir_v1_pir_database.pb.swift
+++ Sources/ApplicationProtobuf/generated/apple_swift_homomorphic_encryption_pir_v1_pir_database.pb.swift
@@ -8,7 +8,7 @@
 // For information on using the generated types, please see the documentation:
 //   https://github.com/apple/swift-protobuf/
 
-// Copyright 2024-2025 Apple Inc. and the Swift Homomorphic Encryption project authors
+// Copyright 2024-2026 Apple Inc. and the Swift Homomorphic Encryption project authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -60,6 +60,34 @@ public struct Apple_SwiftHomomorphicEncryption_Pir_V1_KeywordDatabase: Sendable 
 
   /// List of keyword-value pairs.
   public var rows: [Apple_SwiftHomomorphicEncryption_Pir_V1_KeywordDatabaseRow] = []
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+}
+
+/// Row in an index PIR database.
+public struct Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabaseRow: Sendable {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  /// Value.
+  public var value: Data = Data()
+
+  public var unknownFields = SwiftProtobuf.UnknownStorage()
+
+  public init() {}
+}
+
+/// Input database for index PIR.
+public struct Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabase: Sendable {
+  // SwiftProtobuf.Message conformance is added in an extension below. See the
+  // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
+  // methods supported on all messages.
+
+  /// List of rows.
+  public var rows: [Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabaseRow] = []
 
   public var unknownFields = SwiftProtobuf.UnknownStorage()
 
@@ -129,6 +157,66 @@ extension Apple_SwiftHomomorphicEncryption_Pir_V1_KeywordDatabase: SwiftProtobuf
   }
 
   public static func ==(lhs: Apple_SwiftHomomorphicEncryption_Pir_V1_KeywordDatabase, rhs: Apple_SwiftHomomorphicEncryption_Pir_V1_KeywordDatabase) -> Bool {
+    if lhs.rows != rhs.rows {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabaseRow: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".IndexPirDatabaseRow"
+  public static let _protobuf_nameMap = SwiftProtobuf._NameMap(bytecode: "\0\u{1}value\0")
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeSingularBytesField(value: &self.value) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if !self.value.isEmpty {
+      try visitor.visitSingularBytesField(value: self.value, fieldNumber: 1)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabaseRow, rhs: Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabaseRow) -> Bool {
+    if lhs.value != rhs.value {return false}
+    if lhs.unknownFields != rhs.unknownFields {return false}
+    return true
+  }
+}
+
+extension Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabase: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
+  public static let protoMessageName: String = _protobuf_package + ".IndexPirDatabase"
+  public static let _protobuf_nameMap = SwiftProtobuf._NameMap(bytecode: "\0\u{1}rows\0")
+
+  public mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
+    while let fieldNumber = try decoder.nextFieldNumber() {
+      // The use of inline closures is to circumvent an issue where the compiler
+      // allocates stack space for every case branch when no optimizations are
+      // enabled. https://github.com/apple/swift-protobuf/issues/1034
+      switch fieldNumber {
+      case 1: try { try decoder.decodeRepeatedMessageField(value: &self.rows) }()
+      default: break
+      }
+    }
+  }
+
+  public func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
+    if !self.rows.isEmpty {
+      try visitor.visitRepeatedMessageField(value: self.rows, fieldNumber: 1)
+    }
+    try unknownFields.traverse(visitor: &visitor)
+  }
+
+  public static func ==(lhs: Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabase, rhs: Apple_SwiftHomomorphicEncryption_Pir_V1_IndexPirDatabase) -> Bool {
     if lhs.rows != rhs.rows {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true

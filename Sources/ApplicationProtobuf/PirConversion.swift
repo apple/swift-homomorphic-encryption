cf1e26f5cabf28a39aa261870b3964b044f9649d
Fabian Boemer <fabian_boemer@apple.com>
2026-01-14 16:10:35 -0800

    Remove evaluationKeyConfig from ProcessedDatabaseWithParameters initializer (#266)

--- Sources/ApplicationProtobuf/PirConversion.swift
+++ Sources/ApplicationProtobuf/PirConversion.swift
@@ -1,4 +1,4 @@
-// Copyright 2024-2025 Apple Inc. and the Swift Homomorphic Encryption project authors
+// Copyright 2024-2026 Apple Inc. and the Swift Homomorphic Encryption project authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -188,7 +188,6 @@ extension Apple_SwiftHomomorphicEncryption_Pir_V1_PirParameters {
         try ProcessedDatabaseWithParameters(
             database: database,
             algorithm: algorithm.native(),
-            evaluationKeyConfig: evaluationKeyConfig.native(),
             pirParameter: native(),
             keywordPirParameter: hasKeywordPirParams ? keywordPirParams.native() : nil)
     }
